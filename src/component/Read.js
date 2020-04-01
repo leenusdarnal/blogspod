37a0111ddb1655aa11d63f01ba7bb9d6f4fa2811
Leenus Darnal <leenus.darnal@gmail.com>
2020-04-01 16:44:08 +0530

    added component to display articles

--- src/component/Read.js
+++ src/component/Read.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React ,{Component} from  "react";
+import ArticleCard from "./ArticleCard";
 
-export default class Read extends React.Component {
-    state = {
-        
+export default class Read extends Component {
+    constructor(){
+        super();
+        this.state = {
+            articles:[]
+        }
+    }
+
+    componentDidMount(){
+            fetch("https://blogspod.test.bitpod.io/svc/api/Articles", {
+        headers: {
+        Accept: "application/json",
+        Authorization: "Bearer "+ JSON.parse(localStorage[localStorage[window.$config.oauth.clientId+"lastactiveuserid"]]).authToken,
+        Resolver: "requestHeader",
+        "X-Org-Id": "1"
+        }
+    }).then(response => response.json())
+    .then(response =>{
+        console.log(response);
+        return this.setState({articles:response})  } );
     };
-    render() {
-        return (
-            <div>
-                this is the read tab
+
+    render(){
+        return(
+            <div className="articleListConatiner">
+            
+            <ArticleCard articles={this.state.articles}/>
+
             </div>
         )
     }
