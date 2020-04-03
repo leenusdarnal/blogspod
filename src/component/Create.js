d68470b9ee3252db7992a5437e42b36b09d7bf92
Ahren Pradhan <ahrenpradhan@gmail.com>
2020-04-03 11:00:59 +0530

    integrated axios

--- src/component/Create.js
+++ src/component/Create.js
@@ -1,49 +1,78 @@
 import React, { Component } from 'react';
 import Dante from 'Dante2';
-import {stateToHTML} from 'draft-js-export-html';
+import { stateToHTML } from 'draft-js-export-html';
+import axios from 'axios';
 
 export default class Create extends Component {
     state = {
         mycontent: null
     }
-    content={};
-    // status=false;
+    content = {};
+
+    handlePostArticle = () => {
+        const data = {
+            "UpVote": "0",
+            "DownVote": "0",
+            "Status": "publish",
+            "Title": stateToHTML(this.state.Title.state.editorState._immutable.currentContent),
+            "Content": stateToHTML(this.state.Content.state.editorState._immutable.currentContent),
+            "Tags": document.getElementById('tags').value.split(","),
+            "Email": JSON.parse(localStorage[localStorage[window.$config.oauth.clientId + "lastactiveuserid"]]).userProfile.email
+        }
+
+        const options = {
+            headers: {
+                Accept: "application/json",
+                Authorization: "Bearer " + JSON.parse(localStorage[localStorage[window.$config.oauth.clientId + "lastactiveuserid"]]).authToken,
+                "Content-Type": "application/json",
+                Resolver: "requestHeader", "X-Org-Id": "1"
+            }
+        };
+
+        axios.post('https://blogspod.test.bitpod.io/svc/api/Articles', data, options)
+            .then((res) => {
+                console.log("RESPONSE ==== : ", res);
+            })
+            .catch((err) => {
+                console.log("ERROR: ====", err);
+            })
+    }
     render() {
-        // if(this.status){
-        //     console.log(stateToHTML(this.content))
-        // }
         return (
             <div>
+                <input id="tags" />
                 <Dante
                     body_placeholder={"Title here"}
                     onChange={editor => {
-                        this.setState({ Title: editor,titleObj: stateToHTML(editor.state.editorState._immutable.currentContent)})
+                        this.setState({ Title: editor, titleObj: stateToHTML(editor.state.editorState._immutable.currentContent) })
                     }}
                 />
                 <Dante
                     onChange={editor => {
-                        this.setState({ Content: editor,contentObj: stateToHTML(editor.state.editorState._immutable.currentContent)})
+                        this.setState({ Content: editor, contentObj: stateToHTML(editor.state.editorState._immutable.currentContent) })
                     }}
                 />
 
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-
+                <br />
+                <button onClick={this.handlePostArticle}>Publish article</button>
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+{/* 
                 <hr />
                 {this.state.titleObj}
-                <br/>
+                <br />
                 {this.state.contentObj}
-                <br/>
-                
-                
-                <button onClick={()=>{
+                <br /> */}
+
+
+                {/* <button onClick={() => {
                     console.log(stateToHTML(this.state.Title.state.editorState._immutable.currentContent))
                     console.log(stateToHTML(this.state.Content.state.editorState._immutable.currentContent))
-                }}>click me</button>
+                }}>click me</button> */}
+                <br />
             </div>
         )
     }
