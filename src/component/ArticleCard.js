165e2869d9df8fae4c19ffe6607c5192a4e57ce9
ahrenpradhan <ahrenpradhan@gmail.com>
2020-04-06 17:37:12 +0530

    added tags of articles as chips in read page

--- src/component/ArticleCard.js
+++ src/component/ArticleCard.js
@@ -15,7 +15,14 @@ const ArticleCard = ({ articles }) => {
                             <Route path={"/read"} exact>
                                 <Link to={"/read/article-" + article.id}  style={{ textDecoration: 'none' }}>
                                     <div className="article--container">
-                                        <div className='article--container__title'><strong>{ReactHtmlParser(article.Title)}</strong></div>
+                                        <div className='article--container__header'>
+                                            <div className='article--container__title'><strong>{ReactHtmlParser(article.Title)}</strong></div>
+                                            {/* <div className='article--container__tags'> */}
+                                                <ul className='article--container__tags'>
+                                                    {article.Tags.map(e=>(<li>{e}</li>))}
+                                                </ul>
+                                            {/* </div> */}
+                                        </div>
                                         <div className='article--container__content'>{ReactHtmlParser(article.Content.slice(0,100)+ "...")}</div>
                                         <div className='article--container__content_2'>{ReactHtmlParser(article.Content.slice(0,250)+ "...")}</div>
                                     </div>
